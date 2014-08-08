HackLinux/chromium-src-chrome-browser

commit 8de943baf2493325b972dd89242c9de2ff4617a3

Refactor PermissionsData pt1
In this CL, we:
- Make PermissionsData not-so-static. Only methods which either don't rely on
  knowledge of specific permissions or methods to get/create the object are
  static.
- Separate runtime permissions knowledge from manifest parsing (PermissionsData
  vs PermissionsParser)
- Move required/optional permissions access into PermissionsParser.
- Handle the locking issues.

BUG=379356
TBR=sky@chromium.org (for c/b/ui/, no functional changes)

Review URL: https://codereview.chromium.org/309533007

git-svn-id: http://src.chromium.org/svn/trunk/src/chrome/browser@275142 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

extensions/extension_install_prompt.cc
@@ -708,18 +708,21 @@ void ExtensionInstallPrompt::ShowConfirmation() {
   else
     prompt_.set_experiment(ExtensionInstallPromptExperiment::ControlGroup());
 
-  if (permissions_.get() &&
-      (!extension_ ||
-       !extensions::PermissionsData::ShouldSkipPermissionWarnings(
-           extension_))) {
-    Manifest::Type extension_type = extension_ ?
-        extension_->GetType() : Manifest::TYPE_UNKNOWN;
-    prompt_.SetPermissions(
-        extensions::PermissionMessageProvider::Get()->
-            GetWarningMessages(permissions_, extension_type));
-    prompt_.SetPermissionsDetails(
-        extensions::PermissionMessageProvider::Get()->
-            GetWarningMessagesDetails(permissions_, extension_type));
+  if (permissions_.get()) {
+    if (extension_) {
+      const extensions::PermissionsData* permissions_data =
+          extensions::PermissionsData::ForExtension(extension_);
+      prompt_.SetPermissions(permissions_data->GetPermissionMessageStrings());
+      prompt_.SetPermissionsDetails(
+          permissions_data->GetPermissionMessageDetailsStrings());
+    } else {
+      const extensions::PermissionMessageProvider* message_provider =
+          extensions::PermissionMessageProvider::Get();
+      prompt_.SetPermissions(message_provider->GetWarningMessages(
+          permissions_, Manifest::TYPE_UNKNOWN));
+      prompt_.SetPermissionsDetails(message_provider->GetWarningMessagesDetails(
+          permissions_, Manifest::TYPE_UNKNOWN));
+    }
   }
 
   switch (prompt_.type()) {

apps/ephemeral_app_launcher.cc
@@ -175,7 +175,8 @@ EphemeralAppLauncher::CreateInstallPrompt() const {
   // Skip the prompt by returning null if the app does not need to display
   // permission warnings.
   extensions::PermissionMessages permissions =
-      extensions::PermissionsData::GetPermissionMessages(extension_.get());
+      extensions::PermissionsData::ForExtension(extension_)
+          ->GetPermissionMessages();
   if (permissions.empty())
     return scoped_ptr<ExtensionInstallPrompt::Prompt>();
 

chrome_content_browser_client.cc
@@ -2507,19 +2507,18 @@ bool ChromeContentBrowserClient::AllowPepperSocketAPI(
         extension_set) {
       const Extension* extension = extension_set->GetByID(url.host());
       if (extension) {
+        const extensions::PermissionsData* permissions_data =
+            extensions::PermissionsData::ForExtension(extension);
         if (params) {
           extensions::SocketPermission::CheckParam check_params(
               params->type, params->host, params->port);
-          if (extensions::PermissionsData::CheckAPIPermissionWithParam(
-                  extension, extensions::APIPermission::kSocket,
-                  &check_params)) {
+          if (permissions_data->CheckAPIPermissionWithParam(
+                  extensions::APIPermission::kSocket, &check_params)) {
             return true;
           }
-        } else {
-          if (extensions::PermissionsData::HasAPIPermission(
-                  extension, extensions::APIPermission::kSocket)) {
-            return true;
-          }
+        } else if (permissions_data->HasAPIPermission(
+                       extensions::APIPermission::kSocket)) {
+          return true;
         }
       }
     }

extensions/active_script_controller.cc
@@ -73,10 +73,11 @@ ActiveScriptController* ActiveScriptController::GetForWebContents(
 bool ActiveScriptController::RequiresUserConsentForScriptInjection(
     const Extension* extension) {
   CHECK(extension);
-  if (!PermissionsData::RequiresActionForScriptExecution(
-          extension,
-          SessionID::IdForTab(web_contents()),
-          web_contents()->GetVisibleURL()) ||
+  if (!PermissionsData::ForExtension(extension)
+           ->RequiresActionForScriptExecution(
+               extension,
+               SessionID::IdForTab(web_contents()),
+               web_contents()->GetVisibleURL()) ||
       util::AllowedScriptingOnAllUrls(extension->id(),
                                       web_contents()->GetBrowserContext())) {
     return false;

extensions/active_tab_permission_granter.cc
@@ -43,16 +43,18 @@ void ActiveTabPermissionGranter::GrantIfRequested(const Extension* extension) {
   APIPermissionSet new_apis;
   URLPatternSet new_hosts;
 
+  const PermissionsData* permissions_data =
+      PermissionsData::ForExtension(extension);
+
   // If the extension requires action for script execution, we grant it
   // active tab-style permissions, even if it doesn't have the activeTab
   // permission in the manifest.
   // We don't take tab id into account, because we want to know if the extension
   // should require active tab in general (not for the current tab).
   bool requires_action_for_script_execution =
-      PermissionsData::RequiresActionForScriptExecution(
-          extension,
-          -1,  // No tab id.
-          GURL::EmptyGURL());
+      permissions_data->RequiresActionForScriptExecution(extension,
+                                                         -1,  // No tab id.
+                                                         GURL());
 
   if (extension->HasAPIPermission(APIPermission::kActiveTab) ||
       requires_action_for_script_execution) {
@@ -73,9 +75,7 @@ void ActiveTabPermissionGranter::GrantIfRequested(const Extension* extension) {
     scoped_refptr<const PermissionSet> new_permissions =
         new PermissionSet(new_apis, ManifestPermissionSet(),
                           new_hosts, URLPatternSet());
-    PermissionsData::UpdateTabSpecificPermissions(extension,
-                                                  tab_id_,
-                                                  new_permissions);
+    permissions_data->UpdateTabSpecificPermissions(tab_id_, new_permissions);
     const content::NavigationEntry* navigation_entry =
         web_contents()->GetController().GetVisibleEntry();
     if (navigation_entry) {
@@ -125,7 +125,8 @@ void ActiveTabPermissionGranter::ClearActiveExtensionsAndNotify() {
 
   for (ExtensionSet::const_iterator it = granted_extensions_.begin();
        it != granted_extensions_.end(); ++it) {
-    PermissionsData::ClearTabSpecificPermissions(it->get(), tab_id_);
+    PermissionsData::ForExtension(it->get())
+        ->ClearTabSpecificPermissions(tab_id_);
     extension_ids.push_back((*it)->id());
   }
 

extensions/active_tab_unittest.cc
@@ -107,10 +107,12 @@ class ActiveTabTest : public ChromeRenderViewHostTestHarness {
                  const GURL& url,
                  PermittedFeature feature,
                  int tab_id) {
-    bool script = PermissionsData::CanExecuteScriptOnPage(
-               extension.get(), url, url, tab_id, NULL, -1, NULL);
+    const PermissionsData* permissions_data =
+        PermissionsData::ForExtension(extension);
+    bool script = permissions_data->CanExecuteScriptOnPage(
+        extension, url, url, tab_id, NULL, -1, NULL);
     bool capture = HasTabsPermission(extension, tab_id) &&
-        PermissionsData::CanCaptureVisiblePage(extension.get(), tab_id, NULL);
+                   permissions_data->CanCaptureVisiblePage(tab_id, NULL);
     switch (feature) {
       case PERMITTED_SCRIPT_ONLY:
         return script && !capture;
@@ -142,16 +144,14 @@ class ActiveTabTest : public ChromeRenderViewHostTestHarness {
 
   bool HasTabsPermission(const scoped_refptr<const Extension>& extension,
                          int tab_id) {
-    return PermissionsData::HasAPIPermissionForTab(
-        extension.get(), tab_id, APIPermission::kTab);
+    return PermissionsData::ForExtension(extension)
+        ->HasAPIPermissionForTab(tab_id, APIPermission::kTab);
   }
 
   bool IsGrantedForTab(const Extension* extension,
                        const content::WebContents* web_contents) {
-    return PermissionsData::HasAPIPermissionForTab(
-        extension,
-        SessionID::IdForTab(web_contents),
-        APIPermission::kTab);
+    return PermissionsData::ForExtension(extension)->HasAPIPermissionForTab(
+        SessionID::IdForTab(web_contents), APIPermission::kTab);
   }
 
   // TODO(justinlin): Remove when tabCapture is moved to stable.
@@ -369,16 +369,14 @@ TEST_F(ActiveTabTest, ChromeUrlGrants) {
   // Do not grant tabs/hosts permissions for tab.
   EXPECT_TRUE(IsAllowed(extension_with_tab_capture, internal,
                         PERMITTED_CAPTURE_ONLY));
-  EXPECT_TRUE(PermissionsData::HasAPIPermissionForTab(
-      extension_with_tab_capture.get(),
-      tab_id(),
-      APIPermission::kTabCaptureForTab));
+  const PermissionsData* permissions_data =
+      PermissionsData::ForExtension(extension_with_tab_capture);
+  EXPECT_TRUE(permissions_data->HasAPIPermissionForTab(
+      tab_id(), APIPermission::kTabCaptureForTab));
 
   EXPECT_TRUE(IsBlocked(extension_with_tab_capture, internal, tab_id() + 1));
-  EXPECT_FALSE(PermissionsData::HasAPIPermissionForTab(
-      extension_with_tab_capture.get(),
-      tab_id() + 1,
-      APIPermission::kTabCaptureForTab));
+  EXPECT_FALSE(permissions_data->HasAPIPermissionForTab(
+      tab_id() + 1, APIPermission::kTabCaptureForTab));
 }
 
 }  // namespace

extensions/api/automation_internal/automation_internal_api.cc
@@ -59,7 +59,7 @@ bool CanRequestAutomation(const Extension* extension,
   content::RenderProcessHost* process = contents->GetRenderProcessHost();
   int process_id = process ? process->GetID() : -1;
   std::string unused_error;
-  return PermissionsData::CanExecuteScriptOnPage(
+  return PermissionsData::ForExtension(extension)->CanExecuteScriptOnPage(
       extension, url, url, tab_id, NULL, process_id, &unused_error);
 }
 

extensions/api/autotest_private/autotest_private_api.cc
@@ -34,8 +34,8 @@ namespace {
 base::ListValue* GetHostPermissions(const Extension* ext, bool effective_perm) {
   extensions::URLPatternSet pattern_set;
   if (effective_perm) {
-    pattern_set =
-        extensions::PermissionsData::GetEffectiveHostPermissions(ext);
+    pattern_set = extensions::PermissionsData::ForExtension(ext)
+                      ->GetEffectiveHostPermissions();
   } else {
     pattern_set = ext->GetActivePermissions()->explicit_hosts();
   }

commit 028a0b2613f67b6af9f0886e095fcc96f36759a9

Fix permission regression where warnings aren't shown in the dialog
A regression was introduced in r275142 causing the permissions dialog to not
show permissions. Obviously very bad. Fixed, and added a regression test.

BUG=397900

Review URL: https://codereview.chromium.org/448883002

git-svn-id: http://src.chromium.org/svn/trunk/src/chrome/browser@288365 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

extensions/extension_install_prompt.cc
@@ -711,22 +711,18 @@ void ExtensionInstallPrompt::ShowConfirmation() {
   else
     prompt_->set_experiment(ExtensionInstallPromptExperiment::ControlGroup());
 
-  if (permissions_.get()) {
-    if (extension_) {
-      const extensions::PermissionsData* permissions_data =
-          extension_->permissions_data();
-      prompt_->SetPermissions(permissions_data->GetPermissionMessageStrings());
-      prompt_->SetPermissionsDetails(
-          permissions_data->GetPermissionMessageDetailsStrings());
-    } else {
-      const extensions::PermissionMessageProvider* message_provider =
-          extensions::PermissionMessageProvider::Get();
-      prompt_->SetPermissions(message_provider->GetWarningMessages(
-          permissions_, Manifest::TYPE_UNKNOWN));
-      prompt_->SetPermissionsDetails(
-          message_provider->GetWarningMessagesDetails(permissions_,
-                                                      Manifest::TYPE_UNKNOWN));
-    }
+  if (permissions_.get() &&
+      (!extension_ ||
+       !extensions::PermissionsData::ShouldSkipPermissionWarnings(
+           extension_->id()))) {
+    Manifest::Type type =
+        extension_ ? extension_->GetType() : Manifest::TYPE_UNKNOWN;
+    const extensions::PermissionMessageProvider* message_provider =
+        extensions::PermissionMessageProvider::Get();
+    prompt_->SetPermissions(
+        message_provider->GetWarningMessages(permissions_, type));
+    prompt_->SetPermissionsDetails(
+        message_provider->GetWarningMessagesDetails(permissions_, type));
   }
 
   switch (prompt_->type()) {

extensions/extension_install_prompt.h
@@ -362,6 +362,10 @@ class ExtensionInstallPrompt
   // Installation failed. This is declared virtual for testing.
   virtual void OnInstallFailure(const extensions::CrxInstallerError& error);
 
+  void set_callback_for_test(const ShowDialogCallback& show_dialog_callback) {
+    show_dialog_callback_ = show_dialog_callback;
+  }
+
  protected:
   friend class extensions::ExtensionWebstorePrivateApiTest;
   friend class WebstoreStartupInstallUnpackFailureTest;

extensions/extension_install_prompt_unittest.cc
@@ -1,0 +1,57 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/bind.h"
+#include "base/memory/ref_counted.h"
+#include "base/run_loop.h"
+#include "chrome/browser/extensions/extension_install_prompt.h"
+#include "content/public/test/test_browser_thread_bundle.h"
+#include "extensions/common/extension.h"
+#include "extensions/common/extension_builder.h"
+#include "extensions/common/permissions/api_permission.h"
+#include "extensions/common/permissions/api_permission_set.h"
+#include "extensions/common/permissions/manifest_permission_set.h"
+#include "extensions/common/permissions/permission_set.h"
+#include "extensions/common/url_pattern_set.h"
+#include "extensions/common/value_builder.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace extensions {
+
+void VerifyPromptPermissionsCallback(
+    const base::Closure& quit_closure,
+    const ExtensionInstallPrompt::ShowParams& params,
+    ExtensionInstallPrompt::Delegate* delegate,
+    scoped_refptr<ExtensionInstallPrompt::Prompt> install_prompt) {
+  ASSERT_TRUE(install_prompt.get());
+  EXPECT_EQ(1u, install_prompt->GetPermissionCount());
+  quit_closure.Run();
+}
+
+TEST(ExtensionInstallPromptUnittest, PromptShowsPermissionWarnings) {
+  content::TestBrowserThreadBundle thread_bundle;
+  APIPermissionSet api_permissions;
+  api_permissions.insert(APIPermission::kTab);
+  scoped_refptr<PermissionSet> permission_set =
+      new PermissionSet(api_permissions,
+                        ManifestPermissionSet(),
+                        URLPatternSet(),
+                        URLPatternSet());
+  scoped_refptr<const Extension> extension =
+      ExtensionBuilder().SetManifest(
+          DictionaryBuilder().Set("name", "foo")
+                             .Set("version", "1.0")
+                             .Set("manifest_version", 2)
+                             .Set("description", "Random Ext")).Build();
+  ExtensionInstallPrompt prompt(NULL /* no web contents in this test */);
+  base::RunLoop run_loop;
+  prompt.set_callback_for_test(base::Bind(&VerifyPromptPermissionsCallback,
+                                          run_loop.QuitClosure()));
+  prompt.ConfirmPermissions(NULL,  // no delegate
+                            extension,
+                            permission_set);
+  run_loop.Run();
+}
+
+}  // namespace extensions